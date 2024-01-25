8c52d76fcc127a28d1f7fa948c5764d9bf35672b
YarDol <yardolhushyn@gmail.com>
2024-01-25 14:59:02 +0200

    User page personalised && comments function

--- src/components/page/userPage/userPage.jsx
+++ src/components/page/userPage/userPage.jsx
@@ -1,28 +1,32 @@
 import React, {useEffect, useState} from 'react'
 import PropTypes from 'prop-types'
 import API from '../../../api'
-import QualitiesList from '../../ui/qualities/qualitiesList'
-import { useNavigate } from 'react-router-dom';
+import UserCard from '../../ui/userCard'
+import QualitiesCard from '../../ui/qualitiesCard'
+import MeetingsCard from '../../ui/meetingsCard'
+import Comments from '../../ui/comments'
 
 const UserPage = ({userId}) => {
-    const navigate = useNavigate();
+
     const [user, setUser] = useState()
     useEffect(()=> {
         API.users.getById(userId).then(data => setUser(data))
     },[])
-    const handleClick = () => {
-        navigate(`/users/${userId}/edit`);
-    }
+
     if(user){
         return (
-            <>
-                <h1>{user.name}</h1>
-                <h4>Професія: {user.profession.name}</h4>
-                <QualitiesList qualities={user.qualities} />
-                <p>CompletedMeetings: {user.completedMeetings}</p>
-                <p>Rate: {user.rate}</p>
-                <button onClick={handleClick}>Всі користувачі</button>
-            </>
+            <div className="container">
+                <div className="row gutters-sm">
+                    <div className="col-md-4 mb-3">
+                        <UserCard user={user} />
+                        <QualitiesCard data={user.qualities} />
+                        <MeetingsCard value={user.completedMeetings} />       
+                    </div>
+                    <div className="col-md-8">
+                        <Comments />
+                    </div>
+                </div>
+            </div>
         )
     }else{
        return <h1>Loading</h1>
