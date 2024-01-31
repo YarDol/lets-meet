a5c46bf36bd051152843edc6f585cee61243cb63
YarDol <yardolhushyn@gmail.com>
2024-01-31 13:56:21 +0200

    component refactor

--- src/components/ui/userCard.jsx
+++ src/components/ui/userCard.jsx
@@ -1,43 +1,55 @@
-import React from 'react'
-import PropTypes from 'prop-types'
-import { useNavigate, useLocation } from 'react-router-dom'
-
-const UserCard = ({user}) => {
-    const navigate = useNavigate();
+import React from "react";
+import PropTypes from "prop-types";
+import { useLocation } from "react-router-dom";
+import { useAuth } from "../../hooks/useAuth";
+const UserCard = ({ user }) => {
     const location = useLocation();
+    const { currentUser } = useAuth();
     const handleClick = () => {
-        navigate(`${location.pathname}/edit`);
-    }
+        location(location.pathname + "/edit");
+    };
     return (
         <div className="card mb-3">
             <div className="card-body">
-                <button onClick={handleClick} className="position-absolute top-0 end-0 btn btn-light btn-sm">
-                    <i className="bi bi-gear"></i>
-                </button>
+                {currentUser._id === user._id && (
+                    <button
+                        className="position-absolute top-0 end-0 btn btn-light btn-sm"
+                        onClick={handleClick}
+                    >
+                        <i className="bi bi-gear"></i>
+                    </button>
+                )}
+
                 <div className="d-flex flex-column align-items-center text-center position-relative">
-                    <img src={`https://api.dicebear.com/7.x/avataaars/svg?seed=${(Math.random() + 1).toString(36).substring(7)}`}
-                        className="rounded-circle shadow-1-strong me-3"
-                        alt="avatar"
-                        width="65"
-                        height="65"
+                    <img
+                        src={user.image}
+                        className="rounded-circle"
+                        width="150"
                     />
                     <div className="mt-3">
                         <h4>{user.name}</h4>
-                        <p className="text-secondary mb-1">{user.profession.name}</p>
+                        <p className="text-secondary mb-1">
+                            {user.profession.name}
+                        </p>
                         <div className="text-muted">
-                            <i className="bi bi-caret-down-fill text-primary" role="button"></i>
-                            <i className="bi bi-caret-up text-secondary" role="button"></i>
+                            <i
+                                className="bi bi-caret-down-fill text-primary"
+                                role="button"
+                            ></i>
+                            <i
+                                className="bi bi-caret-up text-secondary"
+                                role="button"
+                            ></i>
                             <span className="ms-2">{user.rate}</span>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
-    )
-}
-
+    );
+};
 UserCard.propTypes = {
     user: PropTypes.object
-}
+};
 
 export default UserCard;
