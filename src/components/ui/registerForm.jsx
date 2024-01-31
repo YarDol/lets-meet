7b3f0a6b2f1c560fef2600f745ab9a59f6dba49f
YarDol <yardolhushyn@gmail.com>
2024-01-31 13:55:17 +0200

    component refactor

--- src/components/ui/registerForm.jsx
+++ src/components/ui/registerForm.jsx
@@ -1,33 +1,45 @@
-import React, {useEffect, useState} from "react";
-import TextField from '../common/form/textField';
-import API from '../../api/'
+import React, { useEffect, useState } from "react";
+import TextField from "../common/form/textField";
 import SelectField from "../common/form/selectField";
 import RadioField from "../common/form/radioField";
-import MultiSelectField from "../common/form/multISelectField";
+import MultiSelectField from "../common/form/multiSelectField";
 import CheckBoxField from "../common/form/checkBoxField";
+import { useQualities } from "../../hooks/useQualities";
+import { useProfessions } from "../../hooks/useProfession";
+import { useAuth } from "../../hooks/useAuth";
+import { useNavigate } from "react-router-dom";
 import * as yup from 'yup';
 
 const RegisterForm = () => {
-    const [data, setData] = useState({email: "", password: "", profession: "", sex: "male", qualities: [], license: false});
+    const navigate = useNavigate();
+    const [data, setData] = useState({
+        email: "",
+        password: "",
+        profession: "",
+        sex: "male",
+        name: "",
+        qualities: [],
+        license: false
+    });
+    const { signUp } = useAuth();
+    const { qualities } = useQualities();
+    const qualitiesList = qualities.map((q) => ({
+        label: q.name,
+        value: q._id
+    }));
+    const { professions } = useProfessions();
+    const professionsList = professions.map((p) => ({
+        label: p.name,
+        value: p._id
+    }));
     const [errors, setErrors] = useState({});
-    const [professions, setProfession] = useState([]);
-    const [qualities, setQualities] = useState({});
-
-    useEffect(() => {
-        API.professions.fetchAll().then((data) => {
-            const professionsList = Object.keys(data).map((professionName) => ({
-                label: data[professionName].name,
-                value: data[professionName]._id
-            }));
-            setProfession(professionsList);
-        });
-        API.qualities.fetchAll().then((data) => setQualities(data))
-    }, []);
 
     const handleChange = (target) => {
-            setData((prevState) => ({...prevState, [target.name]: target.value}))
-    }
-
+        setData((prevState) => ({
+            ...prevState,
+            [target.name]: target.value
+        }));
+    };
     const validateScheme = yup.object().shape({
         license: yup.boolean().oneOf([true],'Need to allow license agreements'),
         profession: yup.string().required("Choose your profession"),
@@ -36,86 +48,104 @@ const RegisterForm = () => {
         .matches(/(?=.*[0-9])/, "Should contain digit")
         .matches(/(?=.*[!@#$%^&*])/, "Should contain at least one of special symbols (!@#$%^&*)")
         .matches(/(?=.{8,})/, "No less then 8 symbols"),
+        name: yup.string().matches(/(?=.{3,})/, "No less then 3 symbols"),
         email: yup.string().required("Email is required").email("Email is incorrect")
     })
 
-    useEffect(()=>{
+    useEffect(() => {
         validate();
-    },[data])
-
+    }, [data]);
     const validate = () => {
         validateScheme.validate(data).then(()=>setErrors({})).catch((err)=>setErrors({[err.path]:err.message}))
         return Object.keys(errors).length === 0
-    }
-
+    };
     const isValid = Object.keys(errors).length === 0;
 
-    const handleSubmit = (e) => {
-        e.preventDefault()
+    const handleSubmit = async (e) => {
+        e.preventDefault();
         const isValid = validate();
-        if(!isValid) return;
-        console.log(data);
-    }
+        if (!isValid) return;
+        const newData = {
+            ...data,
+            qualities: data.qualities.map((q) => q.value)
+        };
+
+        try {
+            await signUp(newData);
+            navigate("/");
+        } catch (error) {
+            setErrors(error);
+        }
+    };
 
     return (
         <form onSubmit={handleSubmit}>
-            <TextField 
-                label="Е-пошта" 
-                name="email" 
-                value={data.email} 
-                onChange={handleChange} 
+            <TextField
+                label="Е-пошта"
+                name="email"
+                value={data.email}
+                onChange={handleChange}
                 error={errors.email}
             />
-            <TextField 
-                label="Пароль" 
-                type="password" 
-                name="password" 
-                value={data.password} 
-                onChange={handleChange} 
+            <TextField
+                label="Ім'я"
+                name="name"
+                value={data.name}
+                onChange={handleChange}
+                error={errors.name}
+            />
+            <TextField
+                label="Пароль"
+                type="password"
+                name="password"
+                value={data.password}
+                onChange={handleChange}
                 error={errors.password}
             />
-            <SelectField 
-                onChange={handleChange} 
-                options={professions} 
-                defaultOption={'Choose...'} 
-                error={errors.profession} 
+            <SelectField
+                label="Оберіть свою професію"
+                defaultOption="Choose..."
+                options={professionsList}
+                name="profession"
+                onChange={handleChange}
                 value={data.profession}
-                name="profession" 
-                label={'Оберіть вашу професію'}
+                error={errors.profession}
             />
             <RadioField
                 options={[
-                    {name:"Male", value: "male"},
-                    {name:"Female", value: "female"},
-                    {name:"Other", value: "other"}
+                    { name: "Male", value: "male" },
+                    { name: "Female", value: "female" },
+                    { name: "Other", value: "other" }
                 ]}
                 value={data.sex}
                 name="sex"
                 onChange={handleChange}
                 label="Оберіть вашу стать"
             />
-            <MultiSelectField 
-                options={qualities}
+            <MultiSelectField
+                options={qualitiesList}
                 onChange={handleChange}
+                defaultValue={data.qualities}
                 name="qualities"
                 label="Оберіть ваші якості"
-                defaultValue = {data.qualities}
             />
-            <CheckBoxField 
+            <CheckBoxField
                 value={data.license}
                 onChange={handleChange}
                 name="license"
                 error={errors.license}
-            > 
+            >
                 Підтвердити <a>ліцензійні умови</a>
             </CheckBoxField>
-            <button 
-                disabled={!isValid} 
-                className='btn btn-primary w-100 mx-auto'>
-                    Submit
+            <button
+                className="btn btn-primary w-100 mx-auto"
+                type="submit"
+                disabled={!isValid}
+            >
+                Submit
             </button>
         </form>
-    )
-}
+    );
+};
 
 export default RegisterForm;
