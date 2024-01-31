8c9a50068a76ae50a4164671f7e0cf68fdf65f6a
YarDol <yardolhushyn@gmail.com>
2024-01-31 13:50:29 +0200

    refactor component

--- src/components/page/editUserPage/editUserPage.jsx
+++ src/components/page/editUserPage/editUserPage.jsx
@@ -3,9 +3,10 @@ import { useNavigate, useParams } from "react-router-dom";
 import API from "../../../api";
 import TextField from "../../common/form/textField";
 import SelectField from "../../common/form/selectField";
-import MultiSelectField from "../../common/form/multISelectField";
+import MultiSelectField from "../../common/form/multiSelectField";
 import RadioField from "../../common/form/radioField";
 import * as yup from 'yup';
+import BackHistoryButton from "../../common/backButton"
 
 const EditUserPage = () => {
     const { userId } = useParams();
@@ -126,58 +127,61 @@ const EditUserPage = () => {
     const isValid = Object.keys(errors).length === 0;
 
     return (
-        <div className='container mt-5'>
-            <div className='row'>
-                <div className='col-md-6 offset-md-3 shadow p-4'>
-                {!isLoading && Object.keys(professions).length > 0 ? (
-                    <form onSubmit={handleSubmit}>
-                        <TextField 
-                            label="Ім'я" 
-                            name="name" 
-                            value={data.name} 
-                            onChange={handleChange} 
-                            error={errors.name}
-                        />
-                        <TextField 
-                            label="Е-пошта" 
-                            name="email" 
-                            value={data.email} 
-                            onChange={handleChange} 
-                            error={errors.email}
-                        />
-                        <SelectField 
-                            onChange={handleChange} 
-                            options={professions} 
-                            defaultOption='Choose...' 
-                            error={errors.profession} 
-                            value={data.profession}
-                            name="professions" 
-                            label='Оберіть вашу професію'
-                        />
-                        <RadioField
-                            options={[
-                                {name:"Male", value: "male"},
-                                {name:"Female", value: "female"},
-                                {name:"Other", value: "other"}
-                            ]}
-                            value={data.sex}
-                            name="sex"
-                            onChange={handleChange}
-                            label="Оберіть вашу стать"
-                        />
-                        <MultiSelectField 
-                            options={qualities}
-                            onChange={handleChange}
-                            name="qualities"
-                            label="Оберіть ваші якості"
-                            defaultValue = {data.qualities}
-                        />
-                        <button 
-                            disabled={!isValid} 
-                            className='btn btn-primary w-100 mx-auto'>
+        <div className="container mt-5">
+            <BackHistoryButton />
+            <div className="row">
+                <div className="col-md-6 offset-md-3 shadow p-4">
+                    {!isLoading && Object.keys(professions).length > 0 ? (
+                        <form onSubmit={handleSubmit}>
+                            <TextField
+                                label="Ім'я"
+                                name="name"
+                                value={data.name}
+                                onChange={handleChange}
+                                error={errors.name}
+                            />
+                            <TextField
+                                label="Е-пошта"
+                                name="email"
+                                value={data.email}
+                                onChange={handleChange}
+                                error={errors.email}
+                            />
+                            <SelectField
+                                label="Оберіть свою професію"
+                                defaultOption="Choose..."
+                                options={professions}
+                                name="profession"
+                                onChange={handleChange}
+                                value={data.profession}
+                                error={errors.profession}
+                            />
+                            <RadioField
+                                options={[
+                                    { name: "Male", value: "male" },
+                                    { name: "Female", value: "female" },
+                                    { name: "Other", value: "other" }
+                                ]}
+                                value={data.sex}
+                                name="sex"
+                                onChange={handleChange}
+                                label="Оберіть вашу стать"
+                            />
+                            <MultiSelectField
+                                defaultValue={data.qualities}
+                                options={qualities}
+                                onChange={handleChange}
+                                name="qualities"
+                                label="Оберіть ваші якості"
+                            />
+                            <button
+                                type="submit"
+                                disabled={!isValid}
+                                className="btn btn-primary w-100 mx-auto"
+                            >
                                 Оновити
-                        </button>
-                    </form>
+                            </button>
+                        </form>
                     ) : (
                         "Loading..."
                     )}
