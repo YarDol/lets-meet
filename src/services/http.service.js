87b8cc5f5ebb9d9719ec05c59016c2ad2eba4d1d
YarDol <yardolhushyn@gmail.com>
2024-01-31 14:03:40 +0200

    component refactor

--- src/services/http.service.js
+++ src/services/http.service.js
@@ -1,28 +1,76 @@
 import axios from "axios";
 import { toast } from "react-toastify";
-import config from '../utils/config.json'
+import configFile from "../utils/config.json";
+import { httpAuth } from "../hooks/useAuth";
+import localStorageService from "./localStorage.service";
 
-axios.defaults.baseURL = config.apiEndpoint;
+const http = axios.create({
+  baseURL: configFile.apiEndpoint,
+});
 
-axios.interceptors.response.use(
-    (res) => res,
-    function (error) {
-        const expectedErrors =
-            error.response &&
-            error.response.status >= 400 &&
-            error.response.status < 500;
+http.interceptors.request.use(
+  async function (config) {
+    if (configFile.isFireBase) {
+      const containSlash = /\/$/gi.test(config.url);
+      config.url =
+        (containSlash ? config.url.slice(0, -1) : config.url) + ".json";
+      const expiresDate = localStorageService.getTokenExpiresDate();
+      const refreshToken = localStorageService.getRefreshToken();
+      if (refreshToken && expiresDate < Date.now()) {
+        const { data } = await httpAuth.post("token", {
+          grant_type: "refresh_token",
+          refresh_token: refreshToken,
+        });
 
-        if (!expectedErrors) {
-            console.log(error);
-            toast.error("Something was wrong. Try it later");
-        }
-        return Promise.reject(error);
+        localStorageService.setTokens({
+          refreshToken: data.refresh_token,
+          idToken: data.id_token,
+          expiresIn: data.expires_id,
+          localId: data.user_id,
+        });
+      }
+      const accessToken = localStorageService.getAccessToken();
+      if (accessToken) {
+        config.params = { ...config.params, auth: accessToken };
+      }
     }
+    return config;
+  },
+  function (error) {
+    return Promise.reject(error);
+  }
+);
+function transformData(data) {
+  return data && !data._id
+    ? Object.keys(data).map((key) => ({
+        ...data[key],
+      }))
+    : data;
+}
+http.interceptors.response.use(
+  (res) => {
+    if (configFile.isFireBase) {
+      res.data = { content: transformData(res.data) };
+    }
+    return res;
+  },
+  function (error) {
+    const expectedErrors =
+      error.response &&
+      error.response.status >= 400 &&
+      error.response.status < 500;
+
+    if (!expectedErrors) {
+      console.log(error);
+      toast.error("Something was wrong. Try it later");
+    }
+    return Promise.reject(error);
+  }
 );
 const httpService = {
-    get: axios.get,
-    post: axios.post,
-    put: axios.put,
-    delete: axios.delete
+  get: http.get,
+  post: http.post,
+  put: http.put,
+  delete: http.delete,
 };
 export default httpService;
