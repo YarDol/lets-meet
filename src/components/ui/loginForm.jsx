1244f28bc2936bf3b441066e4176edace00e158c
YarDol <yardolhushyn@gmail.com>
2024-01-31 13:53:25 +0200

    component optimization

--- src/components/ui/loginForm.jsx
+++ src/components/ui/loginForm.jsx
@@ -1,56 +1,94 @@
-import React, {useEffect, useState} from "react";
-import TextField from '../common/form/textField';
+import React, { useEffect, useState } from "react";
+import TextField from "../common/form/textField";
 import CheckBoxField from "../common/form/checkBoxField";
+import { useAuth } from "../../hooks/useAuth";
+import { useLocation, useNavigate } from "react-router-dom";
 import * as yup from 'yup';
 
 const LoginForm = () => {
-    const [data, setData] = useState({email: "", password: "", stayOn: false});
-
+    const [data, setData] = useState({
+        email: "",
+        password: "",
+        stayOn: false
+    });
+    const navigate = useNavigate();
+    const location = useLocation();
+    const { logIn } = useAuth();
     const [errors, setErrors] = useState({});
-
+    const [enterError, setEnterError] = useState(null);
     const handleChange = (target) => {
-        setData((prevState) => ({...prevState, [target.name]: target.value}))
-    }
-    
+        setData((prevState) => ({
+            ...prevState,
+            [target.name]: target.value
+        }));
+        setEnterError(null);
+    };
+
     const validateScheme = yup.object().shape({
-        password: yup.string().required("Password is required")
-        .matches(/^(?=.*[A-Z])/, "Should contain at least one capital letter")
-        .matches(/(?=.*[0-9])/, "Should contain digit")
-        .matches(/(?=.*[!@#$%^&*])/, "Should contain at least one of special symbols (!@#$%^&*)")
-        .matches(/(?=.{8,})/, "No less then 8 symbols"),
+        password: yup.string().required("Password is required"),
         email: yup.string().required("Email is required").email("Email is incorrect")
     })
 
-    useEffect(()=>{
+    useEffect(() => {
         validate();
-    },[data])
-
+    }, [data]);
     const validate = () => {
         validateScheme.validate(data).then(()=>setErrors({})).catch((err)=>setErrors({[err.path]:err.message}))
         return Object.keys(errors).length === 0
-    }
-
+    };
     const isValid = Object.keys(errors).length === 0;
 
-    const handleSubmit = (e) => {
-        e.preventDefault()
+    const handleSubmit = async (e) => {
+        e.preventDefault();
         const isValid = validate();
-        if(!isValid) return;
-        console.log(data);
-    }
+        if (!isValid) return;
 
+        try {
+            await logIn(data);
+
+            navigate(
+                location.state
+                    ? location.state.from.pathname
+                    : "/"
+            );
+        } catch (error) {
+            setEnterError(error.message);
+        }
+    };
     return (
         <form onSubmit={handleSubmit}>
-            <TextField label="Е-пошта" name="email" value={data.email} onChange={handleChange} error={errors.email}/>
-            <TextField label="Пароль" type="password" name="password" value={data.password} onChange={handleChange} error={errors.password}/>
-            <CheckBoxField 
+            <TextField
+                label="Е-пошта"
+                name="email"
+                value={data.email}
+                onChange={handleChange}
+                error={errors.email}
+            />
+            <TextField
+                label="Пароль"
+                type="password"
+                name="password"
+                value={data.password}
+                onChange={handleChange}
+                error={errors.password}
+            />
+            <CheckBoxField
                 value={data.stayOn}
                 onChange={handleChange}
                 name="stayOn"
-            > Залишитись <a> в системі?</a></CheckBoxField>
-            <button disabled={!isValid} className='btn btn-primary w-100 mx-auto'>Submit</button>
+            >
+                Залишитись в системі?
+            </CheckBoxField>
+            {enterError && <p className="text-danger">{enterError}</p>}
+            <button
+                className="btn btn-primary w-100 mx-auto"
+                type="submit"
+                disabled={!isValid || enterError}
+            >
+                Submit
+            </button>
         </form>
-    )
-}
+    );
+};
 
 export default LoginForm;
