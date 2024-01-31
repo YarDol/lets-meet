1c5b4eef47440c19605c5bb8326fcd0024f2aeb3
YarDol <yardolhushyn@gmail.com>
2024-01-31 13:51:18 +0200

    comments provider added

--- src/components/page/userPage/userPage.jsx
+++ src/components/page/userPage/userPage.jsx
@@ -1,41 +1,39 @@
-import React, {useEffect, useState} from 'react'
-import PropTypes from 'prop-types'
-import API from '../../../api'
-import UserCard from '../../ui/userCard'
-import QualitiesCard from '../../ui/qualitiesCard'
-import MeetingsCard from '../../ui/meetingsCard'
-import Comments from '../../ui/comments'
+import React from "react";
+import PropTypes from "prop-types";
+import UserCard from "../../ui/userCard";
+import QualitiesCard from "../../ui/qualitiesCard";
+import MeetingsCard from "../../ui/meetingsCard";
+import Comments from "../../ui/comments";
+import { useUser } from "../../../hooks/useUsers";
+import { CommentsProvider } from "../../../hooks/useComments";
 
-const UserPage = ({userId}) => {
-
-    const [user, setUser] = useState()
-    useEffect(()=> {
-        API.users.getById(userId).then((data) => setUser(data));
-    },[])
-
-    if(user){
+const UserPage = ({ userId }) => {
+    const { getUserById } = useUser();
+    const user = getUserById(userId);
+    if (user) {
         return (
             <div className="container">
                 <div className="row gutters-sm">
                     <div className="col-md-4 mb-3">
                         <UserCard user={user} />
                         <QualitiesCard data={user.qualities} />
-                        <MeetingsCard value={user.completedMeetings} />       
+                        <MeetingsCard value={user.completedMeetings} />
                     </div>
                     <div className="col-md-8">
-                        <Comments />
+                        <CommentsProvider>
+                            <Comments />
+                        </CommentsProvider>
                     </div>
                 </div>
             </div>
-        )
-    }else{
-       return <h1>Loading</h1>
+        );
+    } else {
+        return <h1>Loading</h1>;
     }
-    
-}
+};
 
 UserPage.propTypes = {
     userId: PropTypes.string.isRequired
-}
+};
 
 export default UserPage;
